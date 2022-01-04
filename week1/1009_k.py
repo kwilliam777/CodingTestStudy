5bc233f9397a08685ad14d1a2e449cd54aeaaf42
Yoon_Chae <yoonchae.na@gmail.com>
2022-01-04 01:22:52 -0600

    jan.4

--- week1/1009_k.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def bitwiseComplement(self, n: int) -> int:
-        test = bin(n)
-        result = test[0:2]
-        for i in range(2,len(test)):
-            result += '0' if test[i] == '1' else '1'
-        return int(result,2) 
