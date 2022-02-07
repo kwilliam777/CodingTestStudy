a0a45b7b82176688025456aa893fc7bfa9ff0759
EungJin Kim <kwilliam777@gmail.com>
2022-02-07 12:57:56 -0600

    feb7

--- week5/k_482_e.py
+++ week5/k_482_e.py
@@ -11,3 +11,16 @@ class Solution:
             if len(result) == 0: result += temp[i*k:(i+1)*k]    
             else: result += "-" + temp[i*k:(i+1)*k]
         return result
+# class Solution:
+#     def licenseKeyFormatting(self, s: str, k: int) -> str:
+#         s=s.replace("-","").upper()
+#         s=s[::-1]
+#         ans=[]
+#         for i in range(0,len(s),k):
+#             (ans.append(s[i:i+k]))
+#             ans.append('-')
+#         if(len(ans)!=0):
+#             ans.pop()
+#         ans=("".join(str(e) for e in  ans))
+#         ans=ans[::-1]
+#         return (ans)
