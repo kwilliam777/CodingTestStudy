9c4b0b5a8bc49ba7668b13dd0e0d0885a09593c2
Eungjin Kim <66508487+kwilliam777@users.noreply.github.com>
2022-02-21 15:19:01 -0600

    feb21

---
+++ weekly assingment/week7/k_141_e.py
@@ -0,0 +1,27 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        def check(head):
+            if head == None: return False
+            if head.val != "check": head.val = "check"
+            elif head.val == "check": return True
+            if head.next != None: return check(head.next)
+        return check(head)
+    
+    
+    
+# class Solution:
+#     def hasCycle(self, head: Optional[ListNode]) -> bool:
+#         if head is None:
+#             return False
+#         slow=fast=head
+#         while (fast.next is not None) and (fast.next.next is not None):
+#                 slow=slow.next
+#                 fast=fast.next.next
+#                 if slow==fast: return True
+#         return False
