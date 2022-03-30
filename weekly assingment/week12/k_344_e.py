e1361415343b05e8fae10d0a7d9f4f69b7febaaf
Eungjin Kim <66508487+kwilliam777@users.noreply.github.com>
2022-03-29 19:32:23 -0500

    mar29

---
+++ weekly assingment/week12/k_344_e.py
@@ -0,0 +1,11 @@
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        s.reverse()
+        
+        
+        
+# class Solution:
+#     def reverseString(self, s: List[str]) -> None:
+#         l = len(s)
+#         for i in range(l//2):
+#             (s[i],s[l-i-1]) = (s[l-i-1],s[i]) 
