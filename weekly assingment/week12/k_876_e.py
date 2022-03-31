6aec97a6d8977f829bda27120e116f7dc59db894
Eungjin Kim <66508487+kwilliam777@users.noreply.github.com>
2022-03-31 14:46:02 -0500

    mar31

---
+++ weekly assingment/week12/k_876_e.py
@@ -0,0 +1,21 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+# class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        save = []
+        while head != None:
+            save.append(head)
+            head = head.next
+        return save[len(save)//2]
+            
+            
+# class Solution:
+#     def middleNode(self, head):
+#         slow = fast = head
+#         while fast and fast.next:
+#             slow = slow.next
+#             fast = fast.next.next
+#         return slow
