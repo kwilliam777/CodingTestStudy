adced368c5386ee5e8ce6b6c8945747ae60911cb
Eungjin Kim <66508487+kwilliam777@users.noreply.github.com>
2022-04-02 15:33:28 -0500

    apr2

---
+++ weekly assingment/week12/k_383_e.py
@@ -0,0 +1,4 @@
+from collections import Counter
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        return True if len(Counter(ransomNote)-Counter(magazine))==0 else False
