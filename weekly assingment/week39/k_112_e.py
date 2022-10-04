11d8c878576aebe6b7e898d66e921ffd840e27a1
Eungjin Kim <66508487+kwilliam777@users.noreply.github.com>
2022-10-04 09:47:01 +0900

    oct4

---
+++ weekly assingment/week39/k_112_e.py
@@ -0,0 +1,10 @@
+class Solution:
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        def check(root, val):
+            if root:
+                if root.left==None and root.right==None:
+                    if val+root.val==targetSum:return True
+                    else:return False
+                else:
+                    return check(root.left,val+root.val) or check(root.right,val+root.val)
+        return check(root,0)
