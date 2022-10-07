5ba7b1f2dfd60d265de2794bcc2e1505db1b3b35
Eungjin Kim <66508487+kwilliam777@users.noreply.github.com>
2022-10-07 23:23:24 +0900

    oct7

---
+++ weekly assingment/week39/k_732_h_x.py
@@ -0,0 +1,19 @@
+from sortedcontainers import SortedDict
+
+class MyCalendarThree:
+    def __init__(self):
+        self.lines = SortedDict()
+
+    def book(self, start: int, end: int) -> int:
+        self.lines[start] = self.lines.get(start, 0) + 1
+        self.lines[end] = self.lines.get(end, 0) - 1
+        # res = accumulate(self.lines.values())
+        # print(self.lines)
+        # print([i for i in res])
+        
+        return max(accumulate(self.lines.values()))
+
+
+# Your MyCalendarThree object will be instantiated and called as such:
+# obj = MyCalendarThree()
+# param_1 = obj.book(start,end)
