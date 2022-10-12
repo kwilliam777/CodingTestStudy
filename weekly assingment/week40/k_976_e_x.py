6a9f4b579369e859b15c8dfed0b7e2ba96d03b5d
Eungjin Kim <66508487+kwilliam777@users.noreply.github.com>
2022-10-12 10:32:16 +0900

    oct12

---
+++ weekly assingment/week40/k_976_e_x.py
@@ -0,0 +1,7 @@
+class Solution:
+    def largestPerimeter(self, A: List[int]) -> int:
+        A.sort(reverse = True)
+        for i in range(3,len(A)+1):
+            if(A[i-3] < A[i-2] + A[i-1]):
+                return sum(A[i-3:i])
+        return 0
